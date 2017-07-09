83d3c579a49cae25ae0f0badc881ca8f00bd7e04
creativezoller <zoltan.belicza@gmail.com>
2017-07-09 17:09:43 +0200

    nunjucks content added, gulptasks modified

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,74 @@ var gulp    = require('gulp'),
         pattern: ['gulp-*', 'gulp.*'],
         replaceString: /\bgulp[\-.]/
     }),
-    taskPath = './gulptasks/',
-    // gConfig = require('./gulptasks/gulp-config'),
-    runSequence = require('run-sequence');
-
+    taskPath    = './gulptasks/',
+    // gConfig     = require('./gulptasks/gulp-config'),
+    fs          = require('fs'),
+    del         = require('del'),
+    runSequence = require('run-sequence'),
+    moment      = require('moment'),
+    tz          = require('moment-timezone');
 function changeEvent(evt) {
-	gulpUtil.log('File', gulpUtil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/./)/'), '')), 'was', gulpUtil.colors.magenta(evt.type));
+	plugins.gulpUtil.log('File', plugins.gulpUtil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/./)/'), '')), 'was', plugins.gulpUtil.colors.magenta(evt.type));
+}
+var manageEnvironment = function(environment) {
+  environment.addFilter('slug', function(str) {
+    return str && str.replace(/\s/g, '-', str).toLowerCase();
+  }); // pl {{ 'My important post'|slug }} >> my-important-post
+  moment.locale('hu');
+  var time = moment().format('LLL').toString();
+  var year = moment().format('YYYY').toString();
+  environment.addGlobal('compileYear', year);
+  environment.addGlobal('compileTime', time);
 }
 
-//TODO: sort them by function to files (eg: generating, linting, fixing, releasing)
-gulp.task('nunjucks', function() {
+gulp.task('clean', function() {
+	del(['.tmp/**','./_dist/**','./_prod/**']);
+});
+gulp.task('nunjucks:generate', function() {
 	plugins.nunjucksRender.nunjucks.configure('./src/nunjucks/templates/');
 	return gulp.src('./src/nunjucks/pages/**/*.+(html|nunjucks)')
 		.pipe(plugins.data(function() {
-			return JSON.parse(plugins.fs.readFileSync('./src/nunjucks/data.json','utf8'))
+			return JSON.parse(fs.readFileSync('./src/nunjucks/data.json','utf8'))
 		}))
-		.pipe(plugins.nunjucksRender())
+		.pipe(plugins.nunjucksRender({
+      manageEnv: manageEnvironment
+    }))
 		.on('error', function(err) {
 			plugins.notify.onError({ title: 'nunjucks error!', message: '<%= error.message %>', sound: 'Frog' })(err);
 			this.emit('end');
 		})
-		.pipe(gulp.dest('./_sites/'));
+		.pipe(gulp.dest('.tmp/_sites/'));
+});
+gulp.task('html:lint', function() {
+	return gulp.src('.tmp/_sites/**/*.html')
+		.pipe(plugins.htmlhint('.htmlhintrc'))
+		.on('error', function(err) {
+			notify.onError({ title: 'Html error!', message: '<%= error.message %>', sound: 'Frog' })(err);
+			this.emit('end');
+		})
+		.pipe(plugins.htmlhint.reporter("htmlhint-stylish")) ;
+});
+gulp.task('html:minify', function() {
+	return gulp.src('.tmp/_sites/**/*.html')
+		.pipe(plugins.htmlmin({
+			removeComments: true,
+			removeCommentsFromCDATA: true,
+			removeCDATASectionsFromCDATA: true,
+			collapseWhitespace: true,
+			collapseInlineTagWhitespace: true,
+			conservativeCollapse: true,
+      keepClosingSlash: true,
+			preserveLineBreaks: true,
+      removeRedundantAttributes: true,
+			removeScriptTypeAttributes: true,
+      sortAttributes: true,
+      sortClassName: true,
+      useShortDoctype: true
+		}))
+		.on('error', function(err) {
+			notify.onError({ title: 'Minify error!', message: '<%= error.message %>', sound: 'Frog' })(err);
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./_dist'));
 });
